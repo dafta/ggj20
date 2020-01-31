1a80b88e6ef2dc45835a0657f2450e15838c804a
matko-dev <matko-dev@github.com>
2020-01-31 21:43:51 +0100

    matko

---
+++ Assets/Matko/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController characterController;
+
+    public Transform pivot;
+    //public Transform pivot2;
+
+    //public Transform playerModel;
+    //public GameObject gunSpawnPoint;
+    //public GameObject player;
+
+    public float CharacterSpeed;
+    public float gravity;
+    public float rotateSpeed = 10;
+    private Vector3 moveDirection;
+
+    float v;
+    float h;
+
+    float mv;
+    float mh;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        mv -= Input.GetAxis("Mouse Y") * 10;
+        mh += Input.GetAxis("Mouse X") * 10;
+
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            CharacterSpeed = 2;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            CharacterSpeed = 1;
+        }
+
+        //animations setup
+        //if ((((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))))
+        //{
+        //    animator.SetBool("isWalking", true);
+        //} else
+        //{
+        //    animator.SetBool("isWalking", false);
+        //}
+
+        //if(Input.GetMouseButton(1))
+        //{
+        //    animator.SetBool("PullGun", true);
+        //    player.transform.rotation = pivot2.transform.rotation;
+        //} 
+       
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    animator.SetBool("PullGun", false);
+        //}
+
+
+        //calculating direction based on which key is pressed (ws - vertically, ad - horizontally)
+        //normalizaing the direction when 2 keys are pressed, gravity and saying move the player in that
+        //calculated direction
+        moveDirection = (transform.forward * v) + (transform.right * h);
+        moveDirection = moveDirection.normalized * CharacterSpeed;
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+         
+
+        //calculating if there is input from the user and if there is than rotate the player in the
+        //direction of the camera and making the player look at the direction of the new rotation which is 
+        //set by user input (wasd), and smoothing that rotation of the player on the line 57
+        if((v != 0) || (h != 0))
+        {
+            transform.rotation = pivot.transform.rotation;
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            //playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
+
---
+++ Assets/Matko/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    //third person controller
+    public float mouseSensitivity = 10f;
+    private float mouseX;
+    private float mouseY;
+
+    public Transform target;
+    public Transform player;
+    public Transform pivot;
+    //public Transform pivot2;
+
+    public float distanceFromTarget;
+    public float rotationSmoothTime = 5f;
+
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        //pivot setup
+        pivot.transform.position = player.transform.position;
+
+        mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(mouseY, mouseX), ref rotationSmoothVelocity, rotationSmoothTime);
+
+        pivot.rotation = Quaternion.Euler(0, mouseX , 0);
+        //pivot2.rotation = Quaternion.Euler(0, mouseX, 0);
+
+        transform.position = target.position - transform.forward * distanceFromTarget;
+        transform.eulerAngles = currentRotation;
+    }
+}
