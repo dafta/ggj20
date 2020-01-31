733e1e3a40c896fd00681ab8461b4fd9babcc8e1
Ivan Barun <ivan13barun@gmail.com>
2020-02-01 00:01:19 +0100

    gj4

--- Assets/Scripts/Ivan/GameManager.cs
+++ Assets/Scripts/Ivan/GameManager.cs
@@ -6,10 +6,54 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool isPaused = false;
+    public GameObject PauseScreen;
+    public Text Timer;
+    private float timer;
+    private int minute;
+    private int sekunde;
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == false)
+            {
+                isPaused = true;
+                Time.timeScale = 0;
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                isPaused = false;
+                Time.timeScale = 1;
+                SceneManager.LoadScene(1);
+            }
+        }
+        minute = (int)timer / 60;
+        sekunde = (int)timer % 60;
+        if(minute < 10)
+        {
+            Timer.text = "0" + minute.ToString() + ":";
+            if (minute < 10)
+                Timer.text += "0" + sekunde;
+            else
+                Timer.text += sekunde;
+        }
+        else
+        {
+            Timer.text = minute.ToString() + ":";
+            if (minute < 10)
+                Timer.text += "0" + sekunde;
+            else
+                Timer.text += sekunde;
+        }
+
     }
 
     public void ExitGame()
