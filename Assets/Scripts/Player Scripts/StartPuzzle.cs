c9e863b8dac68d54580c63361240fe2e4888c39a
Karlo Dautović <dafta007@gmail.com>
2020-02-01 13:24:44 +0100

    Implement puzzle mechanics

---
+++ Assets/Scripts/Player Scripts/StartPuzzle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPuzzle : MonoBehaviour
+{
+    [Header("Text References")]
+    public Text puzzleStartText;
+
+    [Header("Player")]
+    public GameObject player;
+
+    [Header("Puzzles")]
+    public GameObject puzzleTutorial;
+    public GameObject puzzle1;
+    public GameObject puzzle2;
+    public GameObject puzzle3;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PuzzleTutorialStart")
+        {
+            puzzleStartText.text = "Press Q to start puzzle";
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                puzzleStartText.text = "";
+
+                player.SetActive(false);
+                puzzleTutorial.SetActive(true);
+            }
+        }
+
+        if (other.tag == "Puzzle1Start")
+        {
+            puzzleStartText.text = "Press Q to start puzzle";
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                puzzleStartText.text = "";
+
+                player.SetActive(false);
+                puzzle1.SetActive(true);
+            }
+        }
+
+        if (other.tag == "Puzzle2Start")
+        {
+            puzzleStartText.text = "Press Q to start puzzle";
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                puzzleStartText.text = "";
+
+                player.SetActive(false);
+                puzzle2.SetActive(true);
+            }
+        }
+
+        if (other.tag == "Puzzle3Start")
+        {
+            puzzleStartText.text = "Press Q to start puzzle";
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                puzzleStartText.text = "";
+
+                player.SetActive(false);
+                puzzle3.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle/PuzzleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject player;
+
+    [Header("Puzzle")]
+    public GameObject puzzle;
+    public GameObject puzzleStarter;
+
+    private bool exitable = false;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            exitable = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && exitable)
+        {
+            exitable = false;
+
+            puzzle.SetActive(false);
+            player.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        puzzleStarter.SetActive(false);
+        puzzle.SetActive(false);
+        player.SetActive(true);
+    }
+}
