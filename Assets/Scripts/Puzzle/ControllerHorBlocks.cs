b17a7c275417a5cbb7f5109a86960395b85283a8
Karlo Dautović <dafta007@gmail.com>
2020-02-01 11:41:46 +0100

    Added puzzles

---
+++ Assets/Scripts/Puzzle/ControllerHorBlocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerHorBlocks : MonoBehaviour
+{
+    public GameObject hor;
+    public float sensitivity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            hor.GetComponent<Rigidbody>().velocity += new Vector3(0, 0, sensitivity);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            hor.GetComponent<Rigidbody>().velocity -= new Vector3(0, 0, sensitivity);
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle/ControllerVerBlocks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerVerBlocks : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        body.velocity = new Vector3(h * speed, 0, v * speed);
+    }
+}
