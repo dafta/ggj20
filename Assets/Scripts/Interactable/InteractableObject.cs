56c9c919cc6af001596e383e52e8519bb5db49ea
Karlo Dautović <dafta007@gmail.com>
2020-02-01 02:06:32 +0100

    Add basic inventory and repair

---
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string neededItem;
+    public bool repaired = false;
+    public Color repairedColor;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Repair() {
+        repaired = true;
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        renderer.material.color = repairedColor;
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private CharacterController characterController;
+    private List<string> inventory = new List<string>();
+    private GameObject targeted;
 
     public Transform pivot;
     //public Transform pivot2;
@@ -25,14 +27,12 @@ public class PlayerMovement : MonoBehaviour
     float mv;
     float mh;
 
-    private void Start()
-    {
+    private void Start() {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
     }
 
-    void Update()
-    {
+    void Update() {
         mv -= Input.GetAxis("Mouse Y") * 10;
         mh += Input.GetAxis("Mouse X") * 10;
 
@@ -49,6 +49,32 @@ public class PlayerMovement : MonoBehaviour
             CharacterSpeed = 1;
         }
 
+        if (targeted != null
+                && targeted.GetComponent<PickupableObject>() != null
+                && Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("Picking up");
+            inventory.Add(targeted.name);
+            targeted.SetActive(false);
+            targeted = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            Debug.Log("Inventory:");
+            inventory.ForEach(item => Debug.Log(item));
+        }
+
+        if (targeted != null
+                && targeted.GetComponent<InteractableObject>() != null
+                && Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("Repairing");
+            InteractableObject interactable = targeted.GetComponent<InteractableObject>();
+
+            if (!interactable.repaired
+                    && inventory.Contains(interactable.neededItem)) {
+                interactable.Repair();
+            }
+        }
+
         //animations setup
         //if ((((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))))
         //{
@@ -61,9 +87,9 @@ public class PlayerMovement : MonoBehaviour
         //if(Input.GetMouseButton(1))
         //{
         //    animator.SetBool("PullGun", true);
-        //    player.transform.rotation = pivot2.transform.rotation;
-        //} 
-       
+        //    transform.rotation = pivot2.transform.rotation;
+        //}
+
         //if (Input.GetMouseButtonUp(1))
         //{
         //    animator.SetBool("PullGun", false);
@@ -77,10 +103,10 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = moveDirection.normalized * CharacterSpeed;
         moveDirection.y -= gravity * Time.deltaTime;
         characterController.Move(moveDirection * Time.deltaTime);
-         
+
 
         //calculating if there is input from the user and if there is than rotate the player in the
-        //direction of the camera and making the player look at the direction of the new rotation which is 
+        //direction of the camera and making the player look at the direction of the new rotation which is
         //set by user input (wasd), and smoothing that rotation of the player on the line 57
         if((v != 0) || (h != 0))
         {
@@ -89,5 +115,20 @@ public class PlayerMovement : MonoBehaviour
             //playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
     }
-}
 
+    void OnTriggerEnter(Collider trigger) {
+       Debug.Log("Triggered");
+
+       if (trigger.GetComponent<PickupableObject>() != null
+               && trigger.GetComponent<PickupableObject>().pickupable) {
+           Debug.Log("Pickupable");
+           targeted = trigger.gameObject;
+        }
+
+       if (trigger.GetComponent<InteractableObject>() != null
+               && !trigger.GetComponent<InteractableObject>().repaired) {
+           Debug.Log("Interactable");
+           targeted = trigger.gameObject;
+       }
+    }
+}
