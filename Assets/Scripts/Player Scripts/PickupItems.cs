aa0f51f173d735968e7848dd19c99aa8a9707b96
matko-dev <matko-dev@github.com>
2020-02-01 04:09:00 +0100

    Add stuff

---
+++ Assets/Scripts/Player Scripts/PickupItems.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupItems : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject gasTank;
+    public GameObject screwDriver;
+    public GameObject propeler;
+
+    [Header("Text References")]
+    public Text pickUpText;
+
+    private void Start()
+    {
+        gasTank.SetActive(true);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "GasTank")
+        {
+            pickUpText.text = "Press 'E' to pick up gas carnister";
+            if(Input.GetKey(KeyCode.E))
+            {
+                gasTank.SetActive(false);
+                pickUpText.text = "";
+            }
+        }
+
+        if (other.tag == "Propeler")
+        {
+            pickUpText.text = "Press 'E' to pick up wing";
+            if (Input.GetKey(KeyCode.E))
+            {
+                propeler.SetActive(false);
+                pickUpText.text = "";
+            }
+        }
+
+        if (other.tag == "Screwdriver")
+        {
+            pickUpText.text = "Press 'E' to pick up screwdriver";
+            if (Input.GetKey(KeyCode.E))
+            {
+                screwDriver.SetActive(false);
+                pickUpText.text = "";
+            }
+        }
+    }
+}
