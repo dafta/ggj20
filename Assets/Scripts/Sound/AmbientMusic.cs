999f259b7f9ec7c63b6b279fe334c402f501c40e
Ivan Barun <ivan13barun@gmail.com>
2020-02-01 09:20:06 +0100

    gj14

---
+++ Assets/Scripts/Sound/AmbientMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientMusic : MonoBehaviour
+{
+    public AudioSource sound;
+    private bool isPlaying = false;
+    private bool finishedPlaying = false;
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        isPlaying = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlaying == false && finishedPlaying == false)
+        {
+            sound.Play();
+            isPlaying = true;
+        }
+        else if(isPlaying == true &&  finishedPlaying == false)
+        {
+            sound.Stop();
+            finishedPlaying = false;
+        }
+    }
+}
