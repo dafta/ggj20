60e85f80c09676e367551eadf4fe8da3bb8588f5
Karlo Dautović <dafta007@gmail.com>
2020-02-01 02:58:09 +0100

    Add option to stop player movement

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator animator;
-    private CharacterController characterController;
-    private List<string> inventory = new List<string>();
-    private GameObject targeted;
-
     public Transform pivot;
     //public Transform pivot2;
 
@@ -19,7 +14,14 @@ public class PlayerController : MonoBehaviour
     public float CharacterSpeed;
     public float gravity;
     public float rotateSpeed = 10;
+    public bool movementEnabled = true;
+
     private Vector3 moveDirection;
+    private Animator animator;
+    private CharacterController characterController;
+    private GameObject targeted;
+
+    private List<string> inventory = new List<string>();
 
     float v;
     float h;
@@ -33,87 +35,99 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update() {
-        mv -= Input.GetAxis("Mouse Y") * 10;
-        mh += Input.GetAxis("Mouse X") * 10;
+        if (movementEnabled) {
+            mv -= Input.GetAxis("Mouse Y") * 10;
+            mh += Input.GetAxis("Mouse X") * 10;
 
-        v = Input.GetAxis("Vertical");
-        h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
 
-        if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            CharacterSpeed = 2;
-        }
+            if(Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                CharacterSpeed = 2;
+            }
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            CharacterSpeed = 1;
-        }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                CharacterSpeed = 1;
+            }
 
-        if (targeted != null
-                && targeted.GetComponent<PickupableObject>() != null
-                && Input.GetKeyDown(KeyCode.E)) {
-            Debug.Log("Picking up");
-            inventory.Add(targeted.name);
-            targeted.SetActive(false);
-            targeted = null;
-        }
+            if (targeted != null
+                    && targeted.GetComponent<PickupableObject>() != null
+                    && Input.GetKeyDown(KeyCode.E)) {
+                Debug.Log("Picking up");
+                inventory.Add(targeted.name);
+                targeted.SetActive(false);
+                targeted = null;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Tab)) {
-            Debug.Log("Inventory:");
-            inventory.ForEach(item => Debug.Log(item));
-        }
+            if (Input.GetKeyDown(KeyCode.Tab)) {
+                Debug.Log("Inventory:");
+                inventory.ForEach(item => Debug.Log(item));
+            }
 
-        if (targeted != null
-                && targeted.GetComponent<InteractableObject>() != null
-                && Input.GetKeyDown(KeyCode.R)) {
-            Debug.Log("Repairing");
-            InteractableObject interactable = targeted.GetComponent<InteractableObject>();
+            if (targeted != null
+                    && targeted.GetComponent<InteractableObject>() != null
+                    && Input.GetKeyDown(KeyCode.R)) {
+                Debug.Log("Repairing");
+                InteractableObject interactable = targeted.GetComponent<InteractableObject>();
 
-            if (!interactable.repaired
-                    && inventory.Contains(interactable.neededItem)) {
-                inventory.Remove(interactable.neededItem);
-                interactable.Repair();
+                if (!interactable.repaired
+                        && inventory.Contains(interactable.neededItem)) {
+                    inventory.Remove(interactable.neededItem);
+                    interactable.Repair();
+                }
+            }
+
+            //animations setup
+            //if ((((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))))
+            //{
+            //    animator.SetBool("isWalking", true);
+            //} else
+            //{
+            //    animator.SetBool("isWalking", false);
+            //}
+
+            //if(Input.GetMouseButton(1))
+            //{
+            //    animator.SetBool("PullGun", true);
+            //    transform.rotation = pivot2.transform.rotation;
+            //}
+
+            //if (Input.GetMouseButtonUp(1))
+            //{
+            //    animator.SetBool("PullGun", false);
+            //}
+
+
+            //calculating direction based on which key is pressed (ws - vertically, ad - horizontally)
+            //normalizaing the direction when 2 keys are pressed, gravity and saying move the player in that
+            //calculated direction
+            moveDirection = (transform.forward * v) + (transform.right * h);
+            moveDirection = moveDirection.normalized * CharacterSpeed;
+            moveDirection.y -= gravity * Time.deltaTime;
+            characterController.Move(moveDirection * Time.deltaTime);
+
+
+            //calculating if there is input from the user and if there is than rotate the player in the
+            //direction of the camera and making the player look at the direction of the new rotation which is
+            //set by user input (wasd), and smoothing that rotation of the player on the line 57
+            if((v != 0) || (h != 0))
+            {
+                transform.rotation = pivot.transform.rotation;
+                Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+                //playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
             }
         }
 
-        //animations setup
-        //if ((((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))))
-        //{
-        //    animator.SetBool("isWalking", true);
-        //} else
-        //{
-        //    animator.SetBool("isWalking", false);
-        //}
-
-        //if(Input.GetMouseButton(1))
-        //{
-        //    animator.SetBool("PullGun", true);
-        //    transform.rotation = pivot2.transform.rotation;
-        //}
-
-        //if (Input.GetMouseButtonUp(1))
-        //{
-        //    animator.SetBool("PullGun", false);
-        //}
-
-
-        //calculating direction based on which key is pressed (ws - vertically, ad - horizontally)
-        //normalizaing the direction when 2 keys are pressed, gravity and saying move the player in that
-        //calculated direction
-        moveDirection = (transform.forward * v) + (transform.right * h);
-        moveDirection = moveDirection.normalized * CharacterSpeed;
-        moveDirection.y -= gravity * Time.deltaTime;
-        characterController.Move(moveDirection * Time.deltaTime);
-
-
-        //calculating if there is input from the user and if there is than rotate the player in the
-        //direction of the camera and making the player look at the direction of the new rotation which is
-        //set by user input (wasd), and smoothing that rotation of the player on the line 57
-        if((v != 0) || (h != 0))
-        {
-            transform.rotation = pivot.transform.rotation;
-            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-            //playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.F)) {
+            if (movementEnabled) {
+                movementEnabled = !movementEnabled;
+                Cursor.lockState = CursorLockMode.None;
+            } else {
+                movementEnabled = !movementEnabled;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
         }
     }
 
