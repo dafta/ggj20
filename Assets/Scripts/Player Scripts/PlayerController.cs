a4303a00aaa84a4d1f301aec398739a48ae311a5
Ivan Barun <ivan13barun@gmail.com>
2020-02-02 14:47:29 +0100

    gj30

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -131,12 +131,12 @@ public class PlayerController : MonoBehaviour
             //calculating if there is input from the user and if there is than rotate the player in the
             //direction of the camera and making the player look at the direction of the new rotation which is
             //set by user input (wasd), and smoothing that rotation of the player on the line 57
-            if((v != 0) || (h != 0))
-            {
+            //if((v != 0) || (h != 0))
+            //{
                 transform.rotation = pivot.transform.rotation;
                 Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
                 //playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
-            }
+            //}
         }
 
         if (Input.GetKeyDown(KeyCode.F)) {
