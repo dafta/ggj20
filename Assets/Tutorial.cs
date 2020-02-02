04cd627e36bf30260bb77714d0c59ea561e821e3
matko-dev <matko-dev@github.com>
2020-02-02 16:57:56 +0100

    0

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject tutorial;
+
+    void Start()
+    {
+        tutorial.SetActive(true);
+        Invoke("DeleteTutorial", 5f);  
+    }
+    public void DeleteTutorial()
+    {
+        tutorial.SetActive(false);
+    }
+
+}
